825daa01efc480f9c07ddb0c164675f1744c9bbb
Giovani Cerejo Brasil <giovanicerejobrasil@gmail.com>
2023-10-31 11:11:23 -0300

    add reset function

--- src/scripts/engine.js
+++ src/scripts/engine.js
@@ -4,17 +4,39 @@ const state = {
     enemy: document.querySelector(".enemy"),
     timeLeft: document.querySelector("#time-left"),
     score: document.querySelector("#score"),
+    modal: document.querySelector(".modal"),
+    result: document.querySelector("#result"),
+    reset: document.querySelector("#reset"),
   },
   values: {
     hitPosition: 0,
     result: 0,
-    currentTime: 60,
+    currentTime: 10,
+    sleepTime: 1000,
   },
   actions: {
     timerId: setInterval(randomSquare, 1000),
-    countDownTimerId: setInterval(countDown, 800),
+    countDownTimerId: setInterval(countDown, 1000),
   },
 };
+
+function reset() {
+  state.values.hit = 0;
+  state.values.result = 0;
+  state.values.currentTime = 60;
+
+  state.view.modal.classList.add("hide");
+
+  clearInterval(state.actions.timerId);
+  clearInterval(state.actions.countDownTimerId);
+
+  state.actions.timerId = setInterval(randomSquare, state.values.sleepTime);
+  state.actions.countDownTimerId = setInterval(
+    countDown,
+    state.values.sleepTime
+  );
+  state.view.score.textContent = state.values.result;
+}
 
 function countDown() {
   state.values.currentTime--;
@@ -23,7 +45,9 @@ function countDown() {
   if (state.values.currentTime <= 0) {
     clearInterval(state.actions.countDownTimerId);
     clearInterval(state.actions.timerId);
-    alert(`Game Over! O seu resultado foi: ${state.values.result} ponto(s).`);
+
+    state.view.modal.classList.remove("hide");
+    state.view.result.textContent = `${state.values.result} ponto(s).`;
   }
 }
 
@@ -61,6 +85,8 @@ function addListenerHitBox() {
 
 function initialize() {
   addListenerHitBox();
+
+  state.view.reset.addEventListener("click", reset);
 }
 
 initialize();
